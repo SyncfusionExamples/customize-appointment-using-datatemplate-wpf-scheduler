3b2dcf8dd8e4f9b82e06e8f912595deb94cf325b
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-11-30 17:01:44 +0530

    WPF-55774 - Sample and screenshot committed.

---
+++ SchedulerWPF/SchedulerWPF/Helper/SubjectToImageSourceConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfScheduler
+{
+    public class SubjectToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.ToString().Equals("Conference"))
+                return "../Images/Conference_schedule.png";
+            else if (value.ToString().Equals("System Troubleshoot"))
+                return "../Images/Troubleshoot.png";
+            else if (value.ToString().Equals("Checkup"))
+                return "../Images/Stethoscope.png";
+            else
+                return "../Images/cake_schedule.png";
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
+       
+    
+
+
+
